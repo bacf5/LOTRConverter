a8808eb1fec7d50a44f7309039e50213d4fe24e1
bacf <brunoagustincarusofassa@gmail.com>
2025-06-17 12:22:49 +0200

    Initial Commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Bruno Agustín Caruso Fassa on 17/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
