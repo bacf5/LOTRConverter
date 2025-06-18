b84541c8641ad85bb5987586fe55b564efb083f9
bacf <brunoagustincarusofassa@gmail.com>
2025-06-18 16:55:30 +0200

    Added views & SF Symbols & bit of design

--- LOTRConverter/ContentView.swift
+++ LOTRConverter/ContentView.swift
@@ -11,42 +11,56 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             // Background image
+            Image(.background).resizable().ignoresSafeArea()
             
             VStack {
                 // Prancing pony image
                 
+                Image(.prancingpony).resizable().scaledToFit().frame(height: 200)
+                
                 // Currency exchange text
+                
+                Text("Currency Exchange").font(.largeTitle).foregroundStyle(.white)
                 
                 // Conversion section
                 HStack {
                     // Left conversion section
                     VStack {
                          // Currency
-                        Text("hola")
+                    
                         HStack {
                             // currency image
+                            Image(.silverpiece).resizable().scaledToFit().frame(height: 33)
                             
                             // currency text
+                            Text("Silver Piece").font(.headline).foregroundStyle(.white)
                         }
                         // Text field
+                        Text("Text field")
                     }
                     // Equal sign
+                    Image(systemName: "equal").font(.largeTitle).foregroundStyle(.white).symbolEffect(.pulse)
                     
                     // Right conversion section
                     VStack {
                         // Currency
                         HStack {
                             // currency text
-                            
+                            Text("Gold Piece").font(.headline).foregroundStyle(.white)
                             // currency image
+                            Image(.goldpiece).resizable().scaledToFit().frame(height: 33)
                         }
                         // Text field
+                    Text("Text field")
                     }
                 }
                 
+                Spacer()
                 
                 // info Button
+                Image(systemName: "info.circle.fill").font(.largeTitle).foregroundStyle(.white)
             }
+//            .border(.blue)
         }
     }
 }
