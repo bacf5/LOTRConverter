b6b16eb491bf55f295c616b75f0b71a309ec40b3
bacf <brunoagustincarusofassa@gmail.com>
2025-06-21 18:24:39 +0200

    Created ExchangeRate view & reusable component inside the app

--- LOTRConverter/ExchangeInfo.swift
+++ LOTRConverter/ExchangeInfo.swift
@@ -21,14 +21,12 @@ struct ExchangeInfo: View {
                 Text("Here at the Prancing Pony, we are happy to offer you a place where you can exchange all the known currencies in the entire world except one. We used to take Brandy Bucks, but after finding out that it was a person instead of a piece of paper, we realized it had no value to us. Below is a simple guide to our currency exchange rates:").font(.title3).padding()
                 
                 // Exchange rates
-                HStack {
-                    // Left currency image
-                    Image(.goldpiece).resizable().scaledToFit().frame(height: 33)
-                    // Exchange rate text
-                    Text("1 Gold Piece = 4 Gold Pennies") 
-                    // Right currency image
-                    Image(.goldpenny).resizable().scaledToFit().frame(height: 33)
-                }
+                
+                ExchangeRate(leftImage: .goldpiece, text: "1 Gold Piece = 4 Gold Pennies", rightImage: .goldpenny)
+                ExchangeRate(leftImage: .goldpenny, text: "1 Gold Penny = 4 Silver Pieces", rightImage: .silverpiece)
+                ExchangeRate(leftImage: .silverpiece, text: "1 Silver Piece = 4 Silver Pennies", rightImage: .silverpenny)
+                ExchangeRate(leftImage: .silverpenny, text: "1 Silver Penny = 100 Copper Pennies", rightImage: .copperpenny)
+                
                 // Done button
                 Button("Done") {
                 }.buttonStyle(.borderedProminent).tint(.brown.mix(with: .black, by: 0.2)).font(.largeTitle).padding().foregroundStyle(.white)
@@ -41,3 +39,6 @@ struct ExchangeInfo: View {
 #Preview {
     ExchangeInfo()
 }
+
+
+
