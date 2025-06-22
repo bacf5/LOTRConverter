a532fc6569a1dac2e4c7a559b0c54db6fde5a4c0
bacf <brunoagustincarusofassa@gmail.com>
2025-06-22 21:55:08 +0200

    Currency model added

---
+++ LOTRConverter/Currency.swift
@@ -0,0 +1,14 @@
+//
+//  Currency.swift
+//  LOTRConverter
+//
+//  Created by Bruno Agustín Caruso Fassa on 22/06/2025.
+//
+
+enum Currency: Double {
+    case copperPenny = 64000
+    case silverPenny = 64
+    case silverPiece = 16
+    case goldPenny = 4
+    case goldPiece = 1
+}
--- LOTRConverter/SelectCurrency.swift
+++ LOTRConverter/SelectCurrency.swift
@@ -25,7 +25,11 @@ struct SelectCurrency: View {
                     .fontWeight(.bold)
                 
                 // Currency icons
-                CurrencyIcon(currencyImage: .copperpenny, currencyName: "Copper Penny")
+                LazyVGrid(columns: [GridItem(), GridItem(), GridItem()]) {
+                    ForEach(0..<5) { _ in
+                        
+                    }
+                }
                 
                 Text("Select the currency you would like to convert to:")
                     .fontWeight(.bold)
