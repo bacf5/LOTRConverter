ed1a073096f69d1835714f6ddb8b0a68c0ddae19
bacf <brunoagustincarusofassa@gmail.com>
2025-06-22 22:25:14 +0200

    Added conditionals

--- LOTRConverter/Currency.swift
+++ LOTRConverter/Currency.swift
@@ -5,10 +5,37 @@
 //  Created by Bruno Agustín Caruso Fassa on 22/06/2025.
 //
 
+import SwiftUI
+
 enum Currency: Double {
     case copperPenny = 64000
     case silverPenny = 64
     case silverPiece = 16
     case goldPenny = 4
     case goldPiece = 1
+    
+    var image: ImageResource {
+        switch self {
+        case .copperPenny: .copperpenny
+        case .silverPenny: .silverpenny
+        case .silverPiece: .silverpiece
+        case .goldPenny: .goldpenny
+        case .goldPiece: .goldpiece
+            
+        }
+    }
+    var name: String {
+        switch self {
+        case .copperPenny:
+            "Copper Penny"
+        case .silverPenny:
+            "Silver Penny"
+        case .silverPiece:
+            "Silver Piece"
+        case .goldPenny:
+            "Gold Penny"
+        case .goldPiece:
+            "Gold Piece"
+        }
+    }
 }
